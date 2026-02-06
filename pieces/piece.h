fdbe07524ab0eb89a76974415a348f45110c7642
jtechid <techid2000@gmail.com>
2026-02-06 10:58:04 +0700

    typedef function types

--- pieces/piece.h
+++ pieces/piece.h
@@ -10,8 +10,8 @@ typedef struct board_t board_t;
 typedef struct piece_t piece_t;
 typedef struct move_array_t move_array_t;
 
-typedef void (*piece_free_t)(struct piece_t *);
-typedef move_array_t *(*piece_get_moves_t)(piece_t *, board_t *);
+typedef void piece_free_t(struct piece_t *);
+typedef move_array_t *piece_get_moves_t(piece_t *, board_t *);
 
 typedef struct piece_t {
   enum piece_id_t id;
@@ -22,8 +22,8 @@ typedef struct piece_t {
   uint8_t moving_count;
 
   // Virtual functions
-  piece_free_t piece_free;
-  piece_get_moves_t piece_get_moves;
+  piece_free_t *piece_free;
+  piece_get_moves_t *piece_get_moves;
 } piece_t;
 
 uint8_t piece_is_opposite(piece_t *, piece_t *);
